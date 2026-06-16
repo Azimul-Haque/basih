bf805abbf70271cc68218070a9f70974b7b1b200
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-17 03:49:56 +0600

    Commit Wed 06/17/2026  3:49:56.32 8553

--- app/Filament/Widgets/LedgerOverview.php
+++ app/Filament/Widgets/LedgerOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use Filament\Support\Colors\Color;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +11,20 @@ class LedgerOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Total Capital / জমা', '৳ 450,000')
+                ->description('Total cash pool available')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+
+            Stat::make('Active Parties / মোট কাস্টমার', '12')
+                ->description('Registered business profiles')
+                ->descriptionIcon('heroicon-m-users')
+                ->color('info'),
+
+            Stat::make('Pending Expenses / বাকি খরচ', '৳ 18,500')
+                ->description('Unpaid balances due this week')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('danger'),
         ];
     }
 }
