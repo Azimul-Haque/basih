535bca28540598c60d42e7d674bf35a49f7fa5ee
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-17 03:44:45 +0600

    Commit Wed 06/17/2026  3:44:45.31 27613

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
 
-    protected static ?string $modelLabel = '';
+    protected static ?string $modelLabel = 'ব্যবহারকারী তালিকা';
 
     public static function form(Form $form): Form
     {
