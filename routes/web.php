c5f70c0788e9adf449cff12313897bc165635a72
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-17 21:18:34 +0600

    Commit Wed 06/17/2026 21:18:34.24 15582

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clear', ['as'=>'clear','uses'=>'Controller@clear']);
+Route::get('/clear', ['as'=>'clear','uses'=>'Controllers/Controller@clear']);
