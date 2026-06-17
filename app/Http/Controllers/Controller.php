9a8cd16cff4efb91356ba75d88f2025db8391c91
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-17 21:16:54 +0600

    Commit Wed 06/17/2026 21:16:54.82 27203

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function clear()
+    {
+        Artisan::call('optimize:clear');
+        Session::flush();
+        return 'Config and Route Cached. All Cache Cleared';
+    }
 }
