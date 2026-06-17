da51f0daababd41045b329d65ac50252505b95b1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-17 21:21:51 +0600

    Commit Wed 06/17/2026 21:21:51.33 4533

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function clear()
+    {
+        Artisan::call('optimize:clear');
+        Session::flush();
+        return 'Config and Route Cached. All Cache Cleared';
+    }
 }
