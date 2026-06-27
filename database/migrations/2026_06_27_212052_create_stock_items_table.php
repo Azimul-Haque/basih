502d15fc5b74ce069c1222c85d80766adb5e4802
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 21:20:53 +0600

    Commit Sat 06/27/2026 21:20:53.34 24146

--- database/migrations/2026_06_27_212052_create_stock_items_table.php
+++ database/migrations/2026_06_27_212052_create_stock_items_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('stock_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // খাতের নাম
-            $table->enum('type', ['credit', 'debit']); // জমা নাকি খরচ
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('stock_items');
     }
 };
