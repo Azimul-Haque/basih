33a9ec22f3e937826320382aa1912f4819759ba5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 21:31:07 +0600

    Commit Sat 06/27/2026 21:31:07.29 11413

--- app/Models/StockItem.php
+++ app/Models/StockItem.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StockItem extends Model
 {
-    //
+    protected $fillable = [
+        'transaction_id', 'stock_type_id', 'unit_id', 
+        'type', 'quantity', 'extra_cost', 'unit_price', 'profit_per_unit'
+    ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function stockType(): BelongsTo
+    {
+        return $this->belongsTo(StockType::class);
+    }
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
 }
