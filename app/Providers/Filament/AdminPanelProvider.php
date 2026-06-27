f18e4b490826b2f91c11f244432220a32943a297
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-28 02:03:42 +0600

    Commit Sun 06/28/2026  2:03:42.26 11133

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -19,6 +19,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 use App\Filament\Widgets\LedgerOverview;
+use Filament\Pages\Dashboard;
 
 class AdminPanelProvider extends PanelProvider
 {
