1304d3d5b18418377fcc1a7bcf1e70c81df7f73a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 21:21:58 +0600

    Commit Sat 06/27/2026 21:21:58.32 3432

--- database/migrations/2026_06_27_212052_create_stock_items_table.php
+++ database/migrations/2026_06_27_212052_create_stock_items_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('stock_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
+            $table->foreignId('stock_type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('unit_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['buy', 'sell']); // ক্রয় নাকি বিক্রয়
+            $table->decimal('quantity', 12, 2); // পরিমাণ
+            $table->decimal('extra_cost', 10, 2)->default(0.00); // অতিরিক্ত খরচ (পরিবহন/লেবার)
+            $table->decimal('unit_price', 12, 2); // প্রতি এককের প্রকৃত মূল্য
+            $table->decimal('profit_per_unit', 12, 2)->default(0.00); // প্রতি এককে লাভ (শুধুমাত্র বিক্রয়ের সময়)
             $table->timestamps();
         });
     }
