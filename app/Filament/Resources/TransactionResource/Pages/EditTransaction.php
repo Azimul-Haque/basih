acb8e2bb3a4dfc1d4ce584db2f9b8601af13d24b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-28 00:33:28 +0600

    Commit Sun 06/28/2026  0:33:28.31 8536

--- app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
@@ -5,34 +5,6 @@ namespace App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-
-class EditTransaction extends EditRecord
-{
-    protected static string $resource = TransactionResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
-
-    // 🔥 ADD THIS METHOD TO REDIRECT AFTER SAVING EDITS
-    protected function getRedirectUrl(): string
-    {
-        return $this->getResource()::getUrl('index');
-    }
-}
-
-
-
-<?php
-
-namespace App\Filament\Resources\TransactionResource\Pages;
-
-use App\Filament\Resources\TransactionResource;
-use Filament\Actions;
-use Filament\Resources\Pages\EditRecord;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 
