6b7c7606d8839481a28e2d18f09a60e52235f743
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-28 03:04:27 +0600

    Commit Sun 06/28/2026  3:04:27.25 13069

--- app/Filament/Resources/TransactionResource/Pages/ManageDebits.php
+++ app/Filament/Resources/TransactionResource/Pages/ManageDebits.php
@@ -4,10 +4,25 @@ namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
 use Filament\Resources\Pages\Page;
+use Filament\Tables\Concerns\IntersectsWithTables;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 
-class ManageDebits extends Page
+class ManageDebits extends Page implements HasTable
 {
+    use IntersectsWithTables;
+
     protected static string $resource = TransactionResource::class;
 
     protected static string $view = 'filament.resources.transaction-resource.pages.manage-debits';
+
+    protected static ?string $title = 'খরচ খাতা (Debit)';
+
+    public function table(Table $table): Table
+    {
+        // 🔥 মূল টেবিল স্ট্রাকচার কল করে শুধু 'debit' (ক্রয়/খরচ) ডাটা ফিল্টার করা হলো
+        return TransactionResource::table($table)
+            ->modifyQueryUsing(fn ($query) => $query->where('type', 'debit'))
+            ->heading('সকল খরচের তালিকা');
+    }
 }
