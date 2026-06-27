8d25be71e999ec1776ad0b6b5d96a0aecb65d2dd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 22:20:28 +0600

    Commit Sat 06/27/2026 22:20:28.26 2441

--- database/migrations/2026_06_27_222010_add_is_stock_to_categories_table.php
+++ database/migrations/2026_06_27_222010_add_is_stock_to_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->boolean('is_stock')->default(false)->after('type');
         });
     }
 
