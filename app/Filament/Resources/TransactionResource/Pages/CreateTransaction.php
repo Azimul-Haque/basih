443ae084e40165f7d47d52bded01962b7519f2e6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-28 00:32:37 +0600

    Commit Sun 06/28/2026  0:32:37.25 2112

--- app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -5,25 +5,6 @@ namespace App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
-
-class CreateTransaction extends CreateRecord
-{
-    protected static string $resource = TransactionResource::class;
-
-    // 🔥 ADD THIS METHOD TO REDIRECT AFTER CREATING
-    protected function getRedirectUrl(): string
-    {
-        return $this->getResource()::getUrl('index');
-    }
-}
-
-<?php
-
-namespace App\Filament\Resources\TransactionResource\Pages;
-
-use App\Filament\Resources\TransactionResource;
-use Filament\Actions;
-use Filament\Resources\Pages\CreateRecord;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 
