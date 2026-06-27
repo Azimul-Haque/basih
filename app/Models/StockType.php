738144a4400001c6789af74dc12d60ba3c4b4ec4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 21:30:53 +0600

    Commit Sat 06/27/2026 21:30:53.31 8245

--- app/Models/StockType.php
+++ app/Models/StockType.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StockType extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function stockItems(): HasMany
+    {
+        return $this->hasMany(StockItem::class);
+    }
 }
