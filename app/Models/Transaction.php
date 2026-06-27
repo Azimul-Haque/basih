2d24e6479c62a1fc900b3b326abd78fcb42e4b80
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 21:31:00 +0600

    Commit Sat 06/27/2026 21:31:00.31 1228

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['category_id', 'date', 'type', 'amount', 'note'];
+
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function stockItem(): HasOne
+    {
+        return $table->hasOne(StockItem::class);
+    }
 }
