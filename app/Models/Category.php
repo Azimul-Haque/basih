4d39e81013a5c110fa277ba098880c6a799cb292
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-27 22:20:47 +0600

    Commit Sat 06/27/2026 22:20:47.27 20975

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'type'];
+    protected $fillable = ['name', 'type', 'is_stock'];
 
     public function transactions(): HasMany
     {
