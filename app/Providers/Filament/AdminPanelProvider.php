a9904324718cb0c22950fe0d09459246f0ab19ae
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-29 00:04:12 +0600

    Commit Mon 06/29/2026  0:04:12.27 17568

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandLogo(asset('images/favicon.png'))
-            ->brandLogoHeight('2.5rem')
+            ->brandLogoHeight('3rem')
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
